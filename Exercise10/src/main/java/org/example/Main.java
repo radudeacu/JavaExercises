668faba4c006808b0023e2ae1b31ec42e445b776
radudeacu <deacuradu@gmail.com>
2024-02-05 17:59:27 +0200

    Exercise 10, very similar to 9

---
+++ Exercise10/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+/*
+* Write a Java program to compute a specified formula.
+Specified Formula :
+4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+Expected Output
+2.9760461760461765
+* */
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+    }
+}
