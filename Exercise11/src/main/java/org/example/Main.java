522e9ce1166b7cd565ae5a6d5b6708ef2b3a92a5
radudeacu <deacuradu@gmail.com>
2024-02-06 16:20:50 +0200

    Exercise 11  + renaming folders to maintain the order

---
+++ Exercise11/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+/* Write a Java program to print the area and perimeter of a circle.
+Test Data:
+Radius = 7.5
+Expected Output
+Perimeter is = 47.12388980384689
+Area is = 176.71458676442586
+*/
+package org.example;
+
+public class Main {
+
+    // constant for the radius of the circle
+    private static final double radius = 7.5;
+    public static void main(String[] args) {
+
+        // Calculate the perimeter of the circle using the constant radius
+        double perimeter = 2 * Math.PI * radius;
+
+        // Calculate the area of the circle using the constant radius
+        double area = Math.PI * radius * radius;
+
+        // Print the calculated perimeter and area
+        System.out.println("Perimeter is = " + perimeter);
+        System.out.println("Area is = " + area);
+    }
+}
