42aff8ac96327cb1389eca2ba488604a0d81f9dc
radudeacu <deacuradu@gmail.com>
2024-02-28 19:10:55 +0200

    first commit initialized the exercise

---
+++ Exercise18/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+/*
+Write a Java program to multiply two binary numbers.
+Input Data:
+Input the first binary number: 10
+Input the second binary number: 11
+Expected Output
+
+Product of two binary numbers: 110
+*/
+
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
